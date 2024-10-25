34c04f74351f5293b2276e8679caa36229c8fdcf
vae <18137693952@163.com>
2024-10-25 16:21:14 +0800

    feat: 代码格式化

--- cli-progress/index.ts
+++ cli-progress/index.ts
@@ -1,36 +1,35 @@
-import {Bar} from 'cli-progress'
-import pColor from 'picocolors'
-
+import { Bar } from 'cli-progress';
+import pColor from 'picocolors';
 
 // create new progress bar
 const b1 = new Bar({
-  format: 'CLI Progress |' + pColor.redBright('{bar}') + '| {percentage}% || {value}/{total} Chunks || Speed: {speed}',
+  format:
+    'CLI Progress |' +
+    pColor.redBright('{bar}') +
+    '| {percentage}% || {value}/{total} Chunks || Speed: {speed}',
   barCompleteChar: '\u2588',
   barIncompleteChar: '\u2591',
   hideCursor: true
 });
 
-
 const total = 200;
 
 // initialize the bar - defining payload token "speed" with the default value "N/A"
 b1.start(total, 0, {
-  speed: "N/A"
+  speed: 'N/A'
 });
 
 // update values
 b1.increment();
 
-let curNum = 0
+let curNum = 0;
 const timer = setInterval(() => {
-  curNum += 2
+  curNum += 2;
 
-  if (curNum >200) {
-    clearInterval(timer)
+  if (curNum > 200) {
+    clearInterval(timer);
     b1.stop();
   }
 
-  b1.update(curNum, {speed: curNum});
-
-}, 100)
-
+  b1.update(curNum, { speed: curNum });
+}, 100);
